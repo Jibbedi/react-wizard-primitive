37f98c9cf1c0b66829237860adfbcfa8bd5436ab
Johannes Kling <johannes.kling92@gmail.com>
2020-07-09 10:03:12 +0200

    feat: expose useWizardStep hook

---
+++ src/components.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  UseWizard,
+  OnChangeHandler,
+  useWizard,
+  WizardContext,
+  Step,
+  useWizardStep,
+} from "./hooks";
+
+export interface WizardProps {
+  children: ((useWizard: UseWizard) => React.ReactNode) | React.ReactNode;
+  initialStepIndex?: number;
+  onChange?: OnChangeHandler;
+}
+
+export const Wizard: React.FC<WizardProps> = (props: WizardProps) => {
+  const internalState = useWizard({
+    initialStepIndex: props.initialStepIndex,
+    onChange: props.onChange,
+  });
+
+  return (
+    <WizardContext.Provider value={{ ...internalState }}>
+      {typeof props.children === "function"
+        ? props.children(internalState)
+        : props.children}
+    </WizardContext.Provider>
+  );
+};
+
+export interface WizardStepProps {
+  children: (step: Step) => React.ReactNode | any;
+  routeTitle?: string;
+}
+
+export const WizardStep: React.FC<WizardStepProps> = (
+  props: WizardStepProps
+) => {
+  return props.children(useWizardStep({ routeTitle: props.routeTitle })!);
+};
